8f4d69c92af252790d59390ecc09a25868002b28
Anderson Montella <75999604+HerpDerpAndie@users.noreply.github.com>
2024-03-29 15:10:16 +0700

    Meeting 18 start template

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,78 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 
-const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+import { Button } from '../components/ButtonComponent';
+import { Input } from '../components/InputComponent';
 
-export default LoginScreen
+const LoginScreen = () => {
+    return (
+        <ScrollView contentContainerStyle={styles.scroll}>
+            <View style={styles.mainContainer}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={styles.image}
+                        source={
+                          require('../../assets/login.png')
+                        }
+                   />
+               </View>
+               <View style={styles.inputContainer}>
+                  <Input
+                      title="Username"
+                      placeholder="Username"
+                 />
+             </View>
+             <Button
+                 text='Login'
+             />
+             <View style={styles.textContainer}>
+                 <Text style={styles.text}>
+                     Don't have an account?
+                 </Text>
+                 <TouchableOpacity>
+                     <Text style={styles.registerText}>
+                         Register
+                     </Text>
+                 </TouchableOpacity>
+             </View>
+         </View>
+     </ScrollView>
+   )
+};
+
+const styles = StyleSheet.create({
+    scroll: {
+        flexGrow: 1
+    },
+    mainContainer: {
+        backgroundColor: '#E6E6FA',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    imageContainer: {
+        marginTop: 32,
+        marginBottom: 16
+    },
+    image: {
+        width: 180,
+        height: 180
+    },
+    inputContainer: {
+        padding: 16,
+        width: '100%'
+    },
+    textContainer: {
+        flexDirection: 'row',
+        marginTop: 16
+    },
+    text: {
+        fontSize: 16
+    },
+    registerText: {
+        color: '#1A5B0A',
+        fontSize: 16
+    }
+});
+
+export default LoginScreen;
